682752e81764bdbe3ded7d8d2c233a318cd599ef
Saritha R S <Saritha R S@192.168.1.36>
2025-12-16 12:56:28 +0530

    groceryproject

--- groceryproject/src/main/java/utilities/WaitUtilities.java
+++ groceryproject/src/main/java/utilities/WaitUtilities.java
@@ -11,12 +11,26 @@ public class WaitUtilities {
 	public static final int IMPLICITWAIT=5;
 	public static final int EXPLICITWAIT=10;
 	public void waitForElement(WebDriver driver, WebElement target) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(IMPLICITWAIT));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
 		wait.until(ExpectedConditions.visibilityOf(target));
 	}
 	public void waitForElementToBeClickable(WebDriver driver,WebElement element) {
-		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(IMPLICITWAIT));
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
 	    wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	public void waitForAlertIsPresent(WebDriver driver,WebElement element) {
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+	    wait.until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void waitForAttributeContains(WebDriver driver,WebElement element) {
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+	    wait.until(ExpectedConditions.attributeContains(element, null, null));
+	}
+	
+	public void waitForAttributeToBe(WebDriver driver,WebElement element) {
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+	    wait.until(ExpectedConditions.attributeToBe(element, null, null));
 	}
 
 }
